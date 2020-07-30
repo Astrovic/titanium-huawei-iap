7fa59beb883293a97e7c9bf597b04274e3fb3d1f
Prashant Saini <prashant10103497@gmail.com>
2020-07-31 00:03:31 +0530

    feat: hacking class

---
+++ android/src/huawei/iap/hacker/Hack.java
@@ -0,0 +1,53 @@
+package huawei.iap.hacker;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.util.Log;
+
+import com.huawei.hms.support.api.client.Status;
+
+import java.lang.reflect.Field;
+
+public class Hack {
+    public static PendingIntent hackPendingIntent(Status status) {
+        PendingIntent pendingIntent = null;
+        Field[] fields = status.getClass().getDeclaredFields();
+
+        for (Field field : fields) {
+            if (field.getType().getSimpleName().equalsIgnoreCase("PendingIntent")) {
+                try {
+                    field.setAccessible(true);
+                    pendingIntent = (PendingIntent) field.get(status);
+                    Log.i("Hack", "<<< PendingIntent >>> ");
+                }  catch (Exception e) {
+                    e.printStackTrace();
+                    Log.i("Hack", "<<< PendingIntent failed >>> : " + e.getLocalizedMessage());
+                }
+                break;
+            }
+        }
+
+        return pendingIntent;
+    }
+
+    public static Intent hackIntent(Status status) {
+        Intent intent = null;
+        Field[] fields = status.getClass().getDeclaredFields();
+
+        for (Field field : fields) {
+            if (field.getType().getSimpleName().equalsIgnoreCase("Intent")) {
+                try {
+                    field.setAccessible(true);
+                    intent = (Intent) field.get(status);
+                    Log.i("Hack", "<<< Intent >>> ");
+                }  catch (Exception e) {
+                    e.printStackTrace();
+                    Log.i("Hack", "<<< Intent failed >>> : " + e.getLocalizedMessage());
+                }
+                break;
+            }
+        }
+
+        return intent;
+    }
+}
