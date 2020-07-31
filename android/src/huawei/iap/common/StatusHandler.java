a81ef57cbb81dd610df194eb8694cfe74bda2b67
Prashant Saini <prashant10103497@gmail.com>
2020-07-31 16:01:41 +0530

    refactor: use common method for error messages

--- android/src/huawei/iap/common/StatusHandler.java
+++ android/src/huawei/iap/common/StatusHandler.java
@@ -11,46 +11,48 @@ public class StatusHandler {
      * @return String
      */
     public static String getStatusMessage(Exception e) {
-
         if (e instanceof IapApiException) {
             IapApiException iapApiException = (IapApiException) e;
-
-            switch (iapApiException.getStatusCode()) {
-                case OrderStatusCode.ORDER_STATE_CANCEL:
-                    return "Order has been canceled!";
-
-                case OrderStatusCode.ORDER_STATE_PARAM_ERROR:
-                    return "Order state param error!";
-
-                case OrderStatusCode.ORDER_STATE_NET_ERROR:
-                    return "Order state net error!";
-
-                case OrderStatusCode.ORDER_VR_UNINSTALL_ERROR:
-                    return "Order VR uninstall error!";
-
-                case OrderStatusCode.ORDER_HWID_NOT_LOGIN:
-                    return "Login again";
-
-                case OrderStatusCode.ORDER_PRODUCT_OWNED:
-                    return "Product already owned error!";
-
-                case OrderStatusCode.ORDER_PRODUCT_NOT_OWNED:
-                    return "Product not owned error!";
-
-                case OrderStatusCode.ORDER_PRODUCT_CONSUMED:
-                    return "Product consumed error!";
-
-                case OrderStatusCode.ORDER_ACCOUNT_AREA_NOT_SUPPORTED:
-                    return "Order account area not supported error!";
-
-                case OrderStatusCode.ORDER_NOT_ACCEPT_AGREEMENT:
-                    return "User does not agree the agreement";
-
-                default:
-                    return "Order unknown error!";
-            }
+            return getErrorMessage(iapApiException.getStatusCode());
         } else {
             return "external error: " + e.getMessage();
+        }
+    }
+
+    public static String getErrorMessage(int statusCode) {
+        switch (statusCode) {
+            case OrderStatusCode.ORDER_STATE_CANCEL:
+                return "Order has been canceled!";
+
+            case OrderStatusCode.ORDER_STATE_PARAM_ERROR:
+                return "Order state param error!";
+
+            case OrderStatusCode.ORDER_STATE_NET_ERROR:
+                return "Order state net error!";
+
+            case OrderStatusCode.ORDER_VR_UNINSTALL_ERROR:
+                return "Order VR uninstall error!";
+
+            case OrderStatusCode.ORDER_HWID_NOT_LOGIN:
+                return "Login again";
+
+            case OrderStatusCode.ORDER_PRODUCT_OWNED:
+                return "Product already owned error!";
+
+            case OrderStatusCode.ORDER_PRODUCT_NOT_OWNED:
+                return "Product not owned error!";
+
+            case OrderStatusCode.ORDER_PRODUCT_CONSUMED:
+                return "Product consumed error!";
+
+            case OrderStatusCode.ORDER_ACCOUNT_AREA_NOT_SUPPORTED:
+                return "Order account area not supported error!";
+
+            case OrderStatusCode.ORDER_NOT_ACCEPT_AGREEMENT:
+                return "User does not agree the agreement";
+
+            default:
+                return "Order unknown error!";
         }
     }
 }
